286277185e71350542a4ec5b606ad02eb8efb653
Yash <yjain8958372013@gmail.com>
2022-05-14 16:23:45 +0530

    2D Matrix Search

---
+++ Striver/3. Array III/2D-Matrix-Search.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int r = 0,c = n-1;
+        // cout<<r<<" "<<c;
+        while(r<m && c >=0)
+        {
+            if(matrix[r][c]<target)
+                r++;
+            else if (matrix[r][c]>target)
+                c--;
+            else
+                return true;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    return 0;
+}
