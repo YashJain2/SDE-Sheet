c9057cc3ad77180a445be9befefdd0be1aab74e7
Yash <yjain8958372013@gmail.com>
2022-05-15 16:58:54 +0530

    Reverse Pairs in an array

---
+++ Striver/3. Array III/Reverse-pairs.cpp
@@ -0,0 +1,71 @@
+//calculate such pairs in an array such that: j>i && a[i] > 2*a[j]
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int Merge(vector < int > & nums, int low, int mid, int high) {
+  int total = 0;
+  int j = mid + 1;
+  for (int i = low; i <= mid; i++) {
+    while (j <= high && nums[i] > (long long int)(2 * (long long int)nums[j])) {
+      j++;
+    }
+    total += (j - (mid + 1));
+  }
+
+  vector < int > t;
+  int left = low, right = mid + 1;
+
+  while (left <= mid && right <= high) {
+
+    if (nums[left] <= nums[right]) {
+      t.push_back(nums[left++]);
+    } else {
+      t.push_back(nums[right++]);
+    }
+  }
+
+  while (left <= mid) {
+    t.push_back(nums[left++]);
+  }
+  while (right <= high) {
+    t.push_back(nums[right++]);
+  }
+
+  for (int i = low; i <= high; i++) {
+    nums[i] = t[i - low];
+  }
+  return total;
+}
+int MergeSort(vector < int > & nums, int low, int high) {
+
+  if (low >= high) return 0;
+  int mid = (low + high) / 2;
+  int inv = MergeSort(nums, low, mid);
+  inv += MergeSort(nums, mid + 1, high);
+  inv += Merge(nums, low, mid, high);
+  return inv;
+}
+    int reversePairs(vector<int>& nums) {
+        long int ans = 0;
+        //brute force approach
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //     for(int j = i+1; j < nums.size();j++)
+        //     {
+        //         if((long long int)nums[i]>((long long int)nums[j])*2)
+        //             ans++;
+        //     }
+        // }
+        // return ans;
+        int pair = 0;
+        pair = MergeSort(nums,0,nums.size()-1);
+        return pair;
+    }
+};
+
+int main()
+{
+    return 0;
+}
